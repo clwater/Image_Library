ebd4788129d28e0c9a7a96a6e790f5c39975e0dc
clwater <gzb@clwater.top>
2022-02-15 16:04:31 +0800

    WIP(oss): 文件获取

--- app/root.tsx
+++ app/root.tsx
@@ -15,17 +15,12 @@ export const meta: MetaFunction = () => {
 export default function App() {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
       <body>
+        {/* Hi */}
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
+        {/* <ScrollRestoration /> */}
+        {/* <Scripts /> */}
+        {/* {process.env.NODE_ENV === "development" && <LiveReload />} */}
       </body>
     </html>
   );
